1f5154aca3281e8d5777b55fe1b3af38e42bae4f
Ohtico <viticogelvis5@gmail.com>
2021-08-22 19:41:59 -0500

    Progress

---
+++ script/index.js
@@ -0,0 +1,9 @@
+
+function cerrar(){
+    swal.fire({
+        title: 'Estas seguro que quieres cerrar sesion',
+        icon: 'warning',
+        background: `rgba(33, 33, 32)`,
+        confirmButtonText: `Cerrar`
+    })
+}
