6e883aa72d33401bec3c075678b40218c6383c8e
Ohtico <viticogelvis5@gmail.com>
2021-08-27 23:37:54 -0500

    html ya funciona

--- script/index.js
+++ script/index.js
@@ -3,10 +3,12 @@
 let perfil = document.getElementById('perfil');
 let preguntas = ' http://localhost:4001/pregunta1'
 let posicion = 0;
-let wrongAnswer = 0;
+let buenaAnswer = 0;
+let malaAnswer = 0;
+let vida = 4;
+let progreso = 0;
 
-
-function volver(){
+function volver() {
     window.location = "./index.html"
 }
 
@@ -27,18 +29,29 @@ function cerrar() {
 }
 
 async function html(i) {
-    
+
     let resp = await fetch(preguntas)
     let data = await resp.json();
-    
+    if (i == 6 && progreso > 100) {
+        swal.fire({
+            title: 'Felicidades culminaste con exito',
+            showConfirmButton: true,
+            confirmButtonText: 'Volver al inicio'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location = "./index.html";
+            }
+        })
 
-     let q = data[i]; 
-    const a = q.respuesta;
-     rightAnswer= a[0]
-    console.log(rightAnswer)
+    }
 
+    let q = data[i];
+    let a = q.respuesta;
+    rightAnswer = a[0]
+    console.log(data[i])
+    a = a.sort((a, b) => Math.floor(Math.random() * 3) - 1);
     let htmlQuestionCode = `<h7 class="text-white texto m-2 float-end"> ${q.pregunta}</h7>`
-    const htmlAnswers = a.map(currentA => `<button onClick="evaluateAnswer('${currentA}', this)" class="mt-3 btn btn-dark text-start answer"> <span> ${currentA} </span> <input type="radio"></button> ` )
+    const htmlAnswers = a.map(currentA => `<button onClick="evaluateAnswer('${currentA}', this)" class="mt-3 btn btn-dark text-start answer"> <span> ${currentA} </span> <input type="radio"></button> `)
     posicion++;
 
     let body = document.getElementById('body') //linea 32 boton X
@@ -48,10 +61,10 @@ async function html(i) {
                         <button type="button" onclick="volver()" class="btn-close bg-dark" aria-label="Close"></button></div>
                         <div class="col">
                         <div class="progress" style="width: 60vw;">
-                        <div class="progress-bar " role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div></div>
+                        <div class="progress-bar " role="progressbar" style="width:${progreso}%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="2"></div></div>
                         </div>
                         <div class="col col-lg-2">
-                        <h6 class="text-white"><i class="material-icons text-danger justify-content-center">favorite</i>4</h6></div></div></div>
+                        <h6 class="text-white"><i class="material-icons text-danger justify-content-center">favorite</i>${vida}</h6></div></div></div>
                         <div class="container mt-3">
                         <img src="../image/Property 1=1.png" class="float-start mt-5" style="width: 5rem;" alt="HTML5">
                         ${htmlQuestionCode}
@@ -60,27 +73,20 @@ async function html(i) {
 }
 
 const evaluateAnswer = (answer, obj) => {
-    
     document.querySelectorAll('.answer').forEach(a => a.classList.remove('rightCurr', 'wrongCurr'));
-
     const parentP = obj.parentNode;
-   if(answer == rightAnswer ){
-    parentP.classList.add('rightCurr');
-   }else{
-    parentP.classList.add('wrongCurr');
-
-   }
-
+    if (answer == rightAnswer) {
+        parentP.classList.add('rightCurr');
+        progreso = progreso + 17;
+        buenaAnswer++
+    } else {
+        parentP.classList.add('wrongCurr');
+        malaAnswer++
+        vida--
+    }
 }
-const nextQuestion = _ =>{
 
-}
-
-
-
-
-
-function pintarPerfil(){
+function pintarPerfil() {
     let perfilLocal = JSON.parse(localStorage.getItem("usuario"));
     perfil.innerHTML = `<h1 class="text-white">Perfil</h1>
                         <div class="container d-flex justify-content-center p-4">
